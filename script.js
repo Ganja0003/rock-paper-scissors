53e19e0a4dad758c61b59ecf508afe3e46611470
Yuusuf Roble <yuuusuf98@outlook.com>
2023-11-01 11:49:34 +0100

    i have finished the project after long time with some help from a youtube video i will come back to it to make styling some time insha Allah

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const rockButton = document.querySelector('.rock')
 const paperButton = document.querySelector('.paper')
 const scissorsButton = document.querySelector('.scissors')
 const outcomeDiv = document.querySelector('.outcome')
+const playerScoreSpan = document.querySelector('.player-score')
+const computerScoreSpan = document.querySelector('.computer-score')
+const p = document.createElement('p');
+
+
+
 
 let compScore = 0;
 let playerScore = 0;
@@ -16,7 +22,6 @@ const computerPlay = () =>{
 const playRound = (playerSelection,computerSelection) =>{
     if(playerSelection === computerSelection){
         console.log('1',playerSelection, '2', computerSelection)
-        const p = document.createElement('p');
         p.innerText = `you tied! you both picked ${playerSelection}`;
         outcomeDiv.appendChild(p)
     }else if(playerSelection === 'scissors' && computerSelection === 'rock'){
@@ -47,21 +52,49 @@ const playRound = (playerSelection,computerSelection) =>{
 
 }
 
+
+const checkForWinner = (playerScore,compScore) => {
+    if (playerScore === 5){
+        const h2 = document.createElement('h2')
+        h2.classList.add('playerWon')
+        h2.innerHTML = `You won ${playerScore} to ${compScore} great job beating the computer`
+        outcomeDiv.append(h2)
+        outcomeDiv.removeChild(p)
+    }else if(compScore === 5){
+        const h2 = document.createElement('h2')
+        h2.classList.add('CompWon')
+        h2.innerHTML = `You lost ${playerScore} to ${compScore} you got beat by the computer`
+        outcomeDiv.append(h2)
+        outcomeDiv.removeChild(p)
+    }
+}
+
+const updateScores = (playerScore, computerScore) => {
+    playerScoreSpan.innerText = `Player Score: ${playerScore}`
+    computerScoreSpan.innerText = `Computer Score: ${computerScore}`
+}
+
 rockButton.addEventListener('click' , () => {
     const computerSelection = computerPlay();
     const playerSelection = 'rock'
     playRound(playerSelection,computerSelection)
+    updateScores(playerScore,compScore)
+    checkForWinner(playerScore,compScore)
 })
 
 paperButton.addEventListener('click' , () => {
     const computerSelection = computerPlay();
     const playerSelection = 'paper'
     playRound(playerSelection,computerSelection)
+    updateScores(playerScore,compScore)
+    checkForWinner(playerScore,compScore)
 })
 scissorsButton.addEventListener('click' , () => {
     const computerSelection = computerPlay();
     const playerSelection = 'scissors'
     playRound(playerSelection,computerSelection)
+    updateScores(playerScore,compScore)
+    checkForWinner(playerScore,compScore)
 })
 const game = () => {
     //for(let i = 0; i < 5; i++){
